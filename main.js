c3a0f8a6503ba8d68f9754254a0f766b01a77b7c
rjba <117646976+YorNitsuga@users.noreply.github.com>
2024-07-26 15:15:46 +0800

    Add function with win conditions

--- main.js
+++ main.js
@@ -59,4 +59,20 @@ const GameController = (() => {
             }
         }
     };
+
+    const checkWin = () => {
+        const board = Gameboard.getBoard();
+        const winPatterns = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+            [0, 4, 8], [2, 4, 6]             // Diagonals
+        ];
+
+        return winPatterns.some(pattern => {
+            const [a, b, c] = pattern;
+            return board[a] && board[a] === board[b] && board[a] === board[c];
+        });
+    };
+
+    
 })();
